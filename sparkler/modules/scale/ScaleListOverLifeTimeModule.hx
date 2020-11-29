16f45a938f683e2f58dde44d74689afe30c13a43
rudenkoarts <rudenkopaint@gmail.com>
2020-11-29 21:47:02 +0200

    Rename ScaleOverLifeTimeModule to ScaleListOverLifeTimeModule

--- sparkler/modules/scale/ScaleListOverLifeTimeModule.hx
+++ sparkler/modules/scale/ScaleListOverLifeTimeModule.hx
@@ -7,19 +7,19 @@ import sparkler.Particle;
 
 @priority(3)
 @group('scale')
-class ScaleOverLifeTimeModule extends ParticleModule<Particle<Scale, ScalePropertyList>> {
+class ScaleListOverLifeTimeModule extends ParticleModule<Particle<Scale, ScalePropertyList>> {
 
-	public var scaleOverLifeTime:ScalePropertyList;
+	public var scaleListOverLifeTime:ScalePropertyList;
 
 	@filter('_lerp')
 	var _lerp:Float = 0;
 
-	function new(options:{?scaleOverLifeTime:{?ease:(v:Float)->Float, list:Array<{time:Float, value:Float}>}}) {
-		if(options.scaleOverLifeTime != null) {
-			scaleOverLifeTime = ScalePropertyList.create(options.scaleOverLifeTime.list);
-			scaleOverLifeTime.ease = options.scaleOverLifeTime.ease;
+	function new(options:{?scaleListOverLifeTime:{?ease:(v:Float)->Float, list:Array<{time:Float, value:Float}>}}) {
+		if(options.scaleListOverLifeTime != null) {
+			scaleListOverLifeTime = ScalePropertyList.create(options.scaleListOverLifeTime.list);
+			scaleListOverLifeTime.ease = options.scaleListOverLifeTime.ease;
 		} else {
-			scaleOverLifeTime = new ScalePropertyList();
+			scaleListOverLifeTime = new ScalePropertyList();
 		}
 	}
 
@@ -33,7 +33,7 @@ class ScaleOverLifeTimeModule extends ParticleModule<Particle<Scale, ScaleProper
 		p.scale = p.scalePropertyList.value;
 	}
 	override function onParticleSpawn(p:Particle<Scale, ScalePropertyList>) {
-		p.scalePropertyList.set(scaleOverLifeTime);
+		p.scalePropertyList.set(scaleListOverLifeTime);
 		p.scale = p.scalePropertyList.value;
 	}
 	
