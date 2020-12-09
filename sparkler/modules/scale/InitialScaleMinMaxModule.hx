9d1046ff41d272ab76ed7a59933e554eb229038b
rudenkoarts <rudenkopaint@gmail.com>
2020-12-09 04:12:05 +0200

    Create InitialScaleMinMaxModule.hx

---
+++ sparkler/modules/scale/InitialScaleMinMaxModule.hx
@@ -0,0 +1,26 @@
+package sparkler.modules.scale;
+
+import sparkler.components.Scale;
+import sparkler.ParticleModule;
+import sparkler.Particle;
+import sparkler.utils.Bounds;
+
+@priority(3)
+@group('scale')
+class InitialScaleMinMaxModule extends ParticleModule<Particle<Scale>> {
+
+	public var initialScaleMinMax:Bounds<Float>;
+
+	function new(options:{?initialScaleMinMax:{min:Float, max:Float}}) {
+		initialScaleMinMax = new Bounds(1.0, 1.0);
+		if(options.initialScaleMinMax != null) {
+			initialScaleMinMax.min = options.initialScaleMinMax.min;
+			initialScaleMinMax.max = options.initialScaleMinMax.max;
+		}
+	}
+
+	override function onParticleSpawn(p:Particle<Scale>) {
+		p.scale = randomFloat(initialScaleMinMax.min, initialScaleMinMax.max);
+	}
+
+}
